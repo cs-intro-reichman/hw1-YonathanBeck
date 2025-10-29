919ed041000416e1ea370a12a3ae0bdce506e7d6
Yonathan Beck <yonathan.beck@post.runi.ac.il>
2025-10-30 00:58:30 +0200

    completed HW

--- Ascend.java
+++ Ascend.java
@@ -2,6 +2,15 @@
 // and prints them in increasing order.
 public class Ascend {
 	public static void main(String[] args) {
-		// Replace this comment with your code
+		int lim = Integer.parseInt(args[0]);
+		int a = (int)(Math.random() * lim);
+		int b = (int)(Math.random() * lim);
+		int c = (int)(Math.random() * lim);
+		System.out.println(a + " " + b + " " + c);
+		int largest = Math.max(a, Math.max(b, c));
+		int smallest = Math.min(a, Math.min(b, c));;
+		// by adding all the numbers and then subtracting the largest and smallest we get the "remaining" number (the middle number)
+		int middle = a + b + c - largest - smallest;
+		System.out.println(smallest + " " + middle + " " + largest);
 	}
 }
